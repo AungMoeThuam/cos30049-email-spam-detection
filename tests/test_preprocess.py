daf4bc7234a301bb4c12fdd2eeddabba3f3e2107
Show Wai Yan <showwaiyan555@gmail.com>
2026-05-15 11:17:04 +0800

    Fix use of html_text parameter texting by using of self.text

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -2,293 +2,322 @@ from src.preprocess import TextPreprocessor
 
 
 class TestStripHtmlTags:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_removes_simple_tags(self):
-        self.tp.strip_html_tags("<p>hello</p>")
-        assert self.tp.text == "hello"
+        tp = TextPreprocessor("<p>hello</p>")
+        tp.strip_html_tags()
+        assert tp.text == "hello"
 
     def test_removes_nested_tags(self):
-        self.tp.strip_html_tags("<div><b>nested</b></div>")
-        assert self.tp.text == "nested"
+        tp = TextPreprocessor("<div><b>nested</b></div>")
+        tp.strip_html_tags()
+        assert tp.text == "nested"
 
     def test_removes_tags_with_attributes(self):
-        self.tp.strip_html_tags('<a href="link">click</a>')
-        assert self.tp.text == "click"
+        tp = TextPreprocessor('<a href="link">click</a>')
+        tp.strip_html_tags()
+        assert tp.text == "click"
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.strip_html_tags("<p>a</p>")
-        assert result is self.tp
+        tp = TextPreprocessor("<p>a</p>")
+        result = tp.strip_html_tags()
+        assert result is tp
 
     def test_plain_text_unchanged(self):
-        self.tp.strip_html_tags("hello world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello world")
+        tp.strip_html_tags()
+        assert tp.text == "hello world"
 
     def test_empty_string(self):
-        self.tp.strip_html_tags("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.strip_html_tags()
+        assert tp.text == ""
 
     def test_only_tags(self):
-        self.tp.strip_html_tags("<p></p>")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("<p></p>")
+        tp.strip_html_tags()
+        assert tp.text == ""
 
 
 class TestReplacePhoneNumbers:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_us_format_with_dashes(self):
-        self.tp.replace_phone_numbers("call 123-456-7890 now")
-        assert "Phone Number" in self.tp.text
+        tp = TextPreprocessor("call 123-456-7890 now")
+        tp.replace_phone_numbers()
+        assert "Phone Number" in tp.text
 
     def test_us_format_with_parentheses(self):
-        self.tp.replace_phone_numbers("call (123) 456-7890 now")
-        assert "Phone Number" in self.tp.text
+        tp = TextPreprocessor("call (123) 456-7890 now")
+        tp.replace_phone_numbers()
+        assert "Phone Number" in tp.text
 
     def test_with_country_code(self):
-        self.tp.replace_phone_numbers("call +1-123-456-7890 now")
-        assert "Phone Number" in self.tp.text
+        tp = TextPreprocessor("call +1-123-456-7890 now")
+        tp.replace_phone_numbers()
+        assert "Phone Number" in tp.text
 
     def test_with_dots(self):
-        self.tp.replace_phone_numbers("call 123.456.7890 now")
-        assert "Phone Number" in self.tp.text
+        tp = TextPreprocessor("call 123.456.7890 now")
+        tp.replace_phone_numbers()
+        assert "Phone Number" in tp.text
 
     def test_short_number(self):
-        self.tp.replace_phone_numbers("call 555-1234 now")
-        assert "Phone Number" in self.tp.text
+        tp = TextPreprocessor("call 555-1234 now")
+        tp.replace_phone_numbers()
+        assert "Phone Number" in tp.text
 
     def test_no_phone_number(self):
-        self.tp.replace_phone_numbers("hello world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello world")
+        tp.replace_phone_numbers()
+        assert tp.text == "hello world"
 
     def test_empty_string(self):
-        self.tp.replace_phone_numbers("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.replace_phone_numbers()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.replace_phone_numbers("123-456-7890")
-        assert result is self.tp
+        tp = TextPreprocessor("123-456-7890")
+        result = tp.replace_phone_numbers()
+        assert result is tp
 
 
 class TestReplaceUrls:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_http_url(self):
-        self.tp.replace_urls("visit http://example.com now")
-        assert "URL" in self.tp.text
+        tp = TextPreprocessor("visit http://example.com now")
+        tp.replace_urls()
+        assert "URL" in tp.text
 
     def test_https_url(self):
-        self.tp.replace_urls("visit https://example.com now")
-        assert "URL" in self.tp.text
+        tp = TextPreprocessor("visit https://example.com now")
+        tp.replace_urls()
+        assert "URL" in tp.text
 
     def test_url_with_path(self):
-        self.tp.replace_urls("go to https://site.com/page?q=1")
-        assert "URL" in self.tp.text
+        tp = TextPreprocessor("go to https://site.com/page?q=1")
+        tp.replace_urls()
+        assert "URL" in tp.text
 
     def test_no_url(self):
-        self.tp.replace_urls("hello world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello world")
+        tp.replace_urls()
+        assert tp.text == "hello world"
 
     def test_empty_string(self):
-        self.tp.replace_urls("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.replace_urls()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.replace_urls("http://example.com")
-        assert result is self.tp
+        tp = TextPreprocessor("http://example.com")
+        result = tp.replace_urls()
+        assert result is tp
 
 
 class TestNormalizeWhitespace:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_multiple_spaces(self):
-        self.tp.normalize_whitespace("hello    world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello    world")
+        tp.normalize_whitespace()
+        assert tp.text == "hello world"
 
     def test_tabs_and_newlines(self):
-        self.tp.normalize_whitespace("hello\t\tworld\n\nfoo")
-        assert self.tp.text == "hello world foo"
+        tp = TextPreprocessor("hello\t\tworld\n\nfoo")
+        tp.normalize_whitespace()
+        assert tp.text == "hello world foo"
 
     def test_leading_trailing_spaces(self):
-        self.tp.normalize_whitespace("   hello world   ")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("   hello world   ")
+        tp.normalize_whitespace()
+        assert tp.text == "hello world"
 
     def test_single_word(self):
-        self.tp.normalize_whitespace("hello")
-        assert self.tp.text == "hello"
+        tp = TextPreprocessor("hello")
+        tp.normalize_whitespace()
+        assert tp.text == "hello"
 
     def test_empty_string(self):
-        self.tp.normalize_whitespace("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.normalize_whitespace()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.normalize_whitespace("a  b")
-        assert result is self.tp
+        tp = TextPreprocessor("a  b")
+        result = tp.normalize_whitespace()
+        assert result is tp
 
 
 class TestToLowercase:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_uppercase(self):
-        self.tp.to_lowercase("HELLO WORLD")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("HELLO WORLD")
+        tp.to_lowercase()
+        assert tp.text == "hello world"
 
     def test_mixed_case(self):
-        self.tp.to_lowercase("Hello World")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("Hello World")
+        tp.to_lowercase()
+        assert tp.text == "hello world"
 
     def test_already_lower(self):
-        self.tp.to_lowercase("hello world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello world")
+        tp.to_lowercase()
+        assert tp.text == "hello world"
 
     def test_empty_string(self):
-        self.tp.to_lowercase("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.to_lowercase()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.to_lowercase("ABC")
-        assert result is self.tp
+        tp = TextPreprocessor("ABC")
+        result = tp.to_lowercase()
+        assert result is tp
 
 
 class TestRemoveSpecialCharacters:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_removes_listed_chars(self):
-        self.tp.remove_special_characters("~[]'<>(){}\\/!#%^@+=-;.")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("~[]'<>(){}\\/!#%^@+=-;.")
+        tp.remove_special_characters()
+        assert tp.text == ""
 
     def test_preserves_dollar_and_question(self):
-        self.tp.remove_special_characters("hello$world?")
-        assert self.tp.text == "hello$world?"
+        tp = TextPreprocessor("hello$world?")
+        tp.remove_special_characters()
+        assert tp.text == "hello$world?"
 
     def test_preserves_alphanumeric(self):
-        self.tp.remove_special_characters("hello123")
-        assert self.tp.text == "hello123"
+        tp = TextPreprocessor("hello123")
+        tp.remove_special_characters()
+        assert tp.text == "hello123"
 
     def test_preserves_whitespace(self):
-        self.tp.remove_special_characters("a @ b")
-        assert self.tp.text == "a  b"
+        tp = TextPreprocessor("a @ b")
+        tp.remove_special_characters()
+        assert tp.text == "a  b"
 
     def test_empty_string(self):
-        self.tp.remove_special_characters("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.remove_special_characters()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.remove_special_characters("a~b")
-        assert result is self.tp
+        tp = TextPreprocessor("a~b")
+        result = tp.remove_special_characters()
+        assert result is tp
 
 
 class TestReplaceEmojis:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_replaces_money_bag(self):
-        self.tp.replace_emojis("💰")
-        assert ":money_bag:" in self.tp.text
+        tp = TextPreprocessor("💰")
+        tp.replace_emojis()
+        assert ":money_bag:" in tp.text
 
     def test_replaces_multiple_emojis(self):
-        self.tp.replace_emojis("😀👍")
-        assert ":grinning_face:" in self.tp.text
-        assert ":thumbs_up:" in self.tp.text
+        tp = TextPreprocessor("😀👍")
+        tp.replace_emojis()
+        assert ":grinning_face:" in tp.text
+        assert ":thumbs_up:" in tp.text
 
     def test_preserves_plain_text(self):
-        self.tp.replace_emojis("hello world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello world")
+        tp.replace_emojis()
+        assert tp.text == "hello world"
 
     def test_empty_string(self):
-        self.tp.replace_emojis("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.replace_emojis()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.replace_emojis("💰")
-        assert result is self.tp
+        tp = TextPreprocessor("💰")
+        result = tp.replace_emojis()
+        assert result is tp
 
 
 class TestReplaceEmails:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_replaces_simple_email(self):
-        self.tp.replace_emails("contact user@example.com now")
-        assert "EMAIL" in self.tp.text
+        tp = TextPreprocessor("contact user@example.com now")
+        tp.replace_emails()
+        assert "EMAIL" in tp.text
 
     def test_replaces_email_with_dots(self):
-        self.tp.replace_emails("user.name@sub.example.co.uk")
-        assert "EMAIL" in self.tp.text
+        tp = TextPreprocessor("user.name@sub.example.co.uk")
+        tp.replace_emails()
+        assert "EMAIL" in tp.text
 
     def test_replaces_email_with_plus(self):
-        self.tp.replace_emails("user+tag@example.com")
-        assert "EMAIL" in self.tp.text
+        tp = TextPreprocessor("user+tag@example.com")
+        tp.replace_emails()
+        assert "EMAIL" in tp.text
 
     def test_no_email(self):
-        self.tp.replace_emails("hello world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello world")
+        tp.replace_emails()
+        assert tp.text == "hello world"
 
     def test_empty_string(self):
-        self.tp.replace_emails("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.replace_emails()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.replace_emails("a@b.com")
-        assert result is self.tp
+        tp = TextPreprocessor("a@b.com")
+        result = tp.replace_emails()
+        assert result is tp
 
 
 class TestReplacePercentages:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_percentage_without_space(self):
-        self.tp.replace_percentages("increase 50%")
-        assert "PERCENTAGE" in self.tp.text
+        tp = TextPreprocessor("increase 50%")
+        tp.replace_percentages()
+        assert "PERCENTAGE" in tp.text
 
     def test_percentage_with_space(self):
-        self.tp.replace_percentages("increase 50 %")
-        assert "PERCENTAGE" in self.tp.text
+        tp = TextPreprocessor("increase 50 %")
+        tp.replace_percentages()
+        assert "PERCENTAGE" in tp.text
 
     def test_decimal_percentage(self):
-        self.tp.replace_percentages("rate 12.5%")
-        assert "PERCENTAGE" in self.tp.text
+        tp = TextPreprocessor("rate 12.5%")
+        tp.replace_percentages()
+        assert "PERCENTAGE" in tp.text
 
     def test_no_percentage(self):
-        self.tp.replace_percentages("hello world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello world")
+        tp.replace_percentages()
+        assert tp.text == "hello world"
 
     def test_empty_string(self):
-        self.tp.replace_percentages("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.replace_percentages()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.replace_percentages("50%")
-        assert result is self.tp
+        tp = TextPreprocessor("50%")
+        result = tp.replace_percentages()
+        assert result is tp
 
 
 class TestReplaceNumbers:
-    def setup_method(self):
-        self.tp = TextPreprocessor()
-
     def test_replaces_number(self):
-        self.tp.replace_numbers("count 42")
-        assert "NUMBER" in self.tp.text
+        tp = TextPreprocessor("count 42")
+        tp.replace_numbers()
+        assert "NUMBER" in tp.text
 
     def test_replaces_multiple_numbers(self):
-        self.tp.replace_numbers("10 items cost 25 dollars")
-        assert "NUMBER" in self.tp.text
-        assert self.tp.text.count("NUMBER") == 2
+        tp = TextPreprocessor("10 items cost 25 dollars")
+        tp.replace_numbers()
+        assert "NUMBER" in tp.text
+        assert tp.text.count("NUMBER") == 2
 
     def test_no_number(self):
-        self.tp.replace_numbers("hello world")
-        assert self.tp.text == "hello world"
+        tp = TextPreprocessor("hello world")
+        tp.replace_numbers()
+        assert tp.text == "hello world"
 
     def test_empty_string(self):
-        self.tp.replace_numbers("")
-        assert self.tp.text == ""
+        tp = TextPreprocessor("")
+        tp.replace_numbers()
+        assert tp.text == ""
 
     def test_returns_self_for_chaining(self):
-        result = self.tp.replace_numbers("42")
-        assert result is self.tp
-
+        tp = TextPreprocessor("42")
+        result = tp.replace_numbers()
+        assert result is tp
